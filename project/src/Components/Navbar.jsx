ed0d699c850c58f6d2cfc8eaf7733e5bafa672fb
7778862668 <5114saisasale@gmail.com>
2024-07-12 16:28:02 +0530

    homepagestart

--- project/src/Components/Navbar.jsx
+++ project/src/Components/Navbar.jsx
@@ -3,23 +3,25 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
-      <div>
+    <div style={{marginTop:"22px"}}>
+      <div style={{textAlign:"center"}} >
+      <div style={{marginBottom:"10px"}}>
         <Link to={'/'}><img src="https://www.drybar.com/static/version1719476571/frontend/Hot/default/en_US/images/logo.svg" alt="" /></Link>
       </div>
-      <div style={{display:"flex",justifyContent:"space-around"}}>
-        <Link to={'/products'} style={{marginRight:"80px"}}>Hair Product</Link>
-        <Link to={'/hairtools'} style={{marginRight:"80px"}}>Hair Tools</Link>
-        <Link to={'/benefits'} style={{marginRight:"80px"}}>Benefits</Link>
-        <Link to={'/giftsNSets'} style={{marginRight:"80px"}}>Gifts & Sets</Link>
-        <Link to={'/new'} style={{marginRight:"80px"}}>New</Link>
-        <Link to={'/howtoandsets'}>How To & Inspo</Link>
+      <div >
+        <Link to={'/products'} style={{marginRight:"80px",textDecoration:"none",color:"black"}}>Hair Product</Link>
+        <Link to={'/hairtools'} style={{marginRight:"80px",textDecoration:"none",color:"black"}}>Hair Tools</Link>
+        <Link to={'/benefits'} style={{marginRight:"80px",textDecoration:"none",color:"black"}}>Benefits</Link>
+        <Link to={'/giftsNSets'} style={{marginRight:"80px",textDecoration:"none",color:"black"}}>Gifts & Sets</Link>
+        <Link to={'/new'} style={{marginRight:"80px",textDecoration:"none",color:"black"}}>New</Link>
+        <Link to={'/howtoandsets'} style={{textDecoration:"none",color:"black"}}>How To & Inspo</Link>
       </div>
-      <div>
-        <Link to={''} style={{marginRight:"50px"}}>Free Shipping on all purchase</Link>
-        <Link to={''} style={{marginRight:"50px"}}>60 days Return Policy</Link>
-        <Link to={''} style={{marginRight:"50px"}}>Subscribe For Specials Offer</Link>
+      <div style={{backgroundColor:"gray",marginBottom:"10px",marginTop:"20px",padding:"20px"}}>
+        <Link to={''} style={{marginRight:"50px",textDecoration:"none",color:"black"}}>Free Shipping on all purchase</Link>
+        <Link to={''} style={{marginRight:"50px",textDecoration:"none",color:"black"}}>60 days Return Policy</Link>
+        <Link to={''} style={{marginRight:"50px",textDecoration:"none",color:"black"}}>Subscribe For Specials Offer</Link>
 
+      </div>
       </div>
     </div>
   )
--- project/src/Pages/Home.jsx
+++ project/src/Pages/Home.jsx
@@ -1,15 +1,132 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
 import Carousel from 'react-bootstrap/Carousel';
-
-
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const Home = () => {
+  
   return (
-    
-      <div>
-        <h1>HOME CAREOSEL</h1>
+    <div >
+      <div style={{marginBottom:"30px",marginTop:"40px"}}>
+      <Carousel data-bs-theme="dark">
+      <Carousel.Item>
+        <Row>
+          <Col><img
+          className="d-block w-100"
+          src="https://www.drybar.com/media/wysiwyg/definition_widget/LGCollection_2024_DefinitionWidget_D900x540.jpg"
+          alt="First slide"
+          height={"500px"}
+        /></Col>
+          <Col style={{backgroundColor:"lightskyblue"}}>
+            <div style={{textAlign:"center",marginTop:"170px"}}>
+                <h1>STOP FRIZE BEFORE IT STARTS</h1>
+                <h6>Shop Our Tried And True Fizz Product </h6><br /><br />
+                <button>Shop Now</button>
+            </div>
+          </Col>
+        </Row><br /><br /><br /><br />
+        
+      </Carousel.Item>
+      <Carousel.Item>
+        <Row>
+            <Col><img
+            className="d-block w-100"
+            src="https://www.drybar.com/media/wysiwyg/definition_widget/DefWidget_ShotCollection_D900x540.jpg"
+            alt="First slide"
+            height={"500px"}
+          /></Col>
+            <Col style={{backgroundColor:"lightyellow"}}>
+            <div style={{textAlign:"center",marginTop:"170px"}}>
+                <h1>STOP FRIZE BEFORE IT STARTS</h1>
+                <h6>Shop Our Tried And True Fizz Product </h6><br /><br />
+                <button>Shop Now</button>
+            </div>
+          </Col>
+          </Row><br /><br /><br /><br />
+      </Carousel.Item>
+      <Carousel.Item>
+          <Row>
+              <Col><img
+              className="d-block w-100"
+              src="https://www.drybar.com/media/wysiwyg/definition_widget/BackToSchool_DefinitionWidget2_764x789.jpg"
+              alt="First slide"
+              height={"500px"}
+            /></Col>
+              <Col style={{backgroundColor:"lightskyblue"}}>
+            <div style={{textAlign:"center",marginTop:"170px"}}>
+                <h1>STOP FRIZE BEFORE IT STARTS</h1>
+                <h6>Shop Our Tried And True Fizz Product </h6><br /><br />
+                <button>Shop Now</button>
+            </div>
+          </Col>
+          </Row><br /><br /><br /><br />
+      </Carousel.Item>
+    </Carousel>
       </div>
- 
+      
+        <div style={{display:"flex",justifyContent:"center",width:"50%"}}>
+          <h4>SHOP OUR BEST SELLERS</h4>
+        </div>
+
+        <div>
+          <img src="https://www.drybar.com/media/wysiwyg/hero_banner_D1365x439/MadeWithout_2023_SlimBanner_D1465x300_002_.jpg" alt="" style={{width:"100%"}}/>
+        </div>
+
+
+
+        <div style={{display:"flex",justifyContent:"center",margin:"auto",width:"100%"}}>
+          <div style={{textAlign:"center"}}>
+            <div>
+            <h1>Bestseller Spotlight: Detox Takeover</h1>
+            <h3>Share your photos with us! @thedrybar</h3>
+            </div>
+            <div>
+              <h1>careosel</h1>
+            </div>
+          </div>
+        </div>
+
+
+        <div style={{width:"100%",paddingBottom:"5px"}}>
+       
+            <Row >
+            <Col style={{backgroundColor:"lightgray",display:"flex",justifyContent:"space-between"}}>
+            <div style={{textAlign:"center",marginTop:"170px",marginLeft:"180px"}}>
+                <h1>STOP FRIZE BEFORE IT STARTS</h1>
+                <h6>Shop Our Tried And True Fizz Product </h6><br /><br />
+                <button>Shop Now</button>
+            </div>
+          <img
+          className="d-block w-50"
+          src="https://www.drybar.com/media//wysiwyg/definition_widget/ReserveCollection_DefinitionWidget_D900x540.jpg"
+          alt="First slide"
+          height={"500px"}
+        /></Col>
+            </Row> 
+        </div>
+
+
+        <div style={{width:"100%",paddingBottom:"10px"}}>
+       
+            <Row >
+              <Col style={{backgroundColor:"#fa8072",display:"flex"}}><img
+                        className="d-block w-50"
+                        src="https://www.drybar.com/media//wysiwyg/definition_widget/SeltzerSpritzRumPunch_2024_DefinitionWidget1_D900x540.jpg"
+                        alt="First slide"
+                        height={"500px"}
+                      />
+                          <div style={{textAlign:"center",marginTop:"170px",marginLeft:"180px"}}>
+                              <h1>STOP FRIZE BEFORE IT STARTS</h1>
+                              <h6>Shop Our Tried And True Fizz Product </h6><br /><br />
+                              <button>Shop Now</button>
+                          </div>
+                        </Col>
+            </Row> 
+        </div>
+    
+    </div>
   )
 }
 
